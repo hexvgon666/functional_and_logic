563132d08afa7a73e04dbb870f43f9e7e73fa4c5
Мищенко Н <147876227+hexvgon666@users.noreply.github.com>
2024-03-22 10:23:59 +0300

    Лаб_1 Задание 4 - В акинатор добавлены изменения
    Добавлены вопросы increasing_of_temperature, no_audio, program_closes, productivity_drops_sharply и объекты audio_card_broken, computer_mining с добавлением вопросов и computer_wont_turn_on, system_overheating без добавления вопросов

---
+++ LR1/akinator.pl
@@ -0,0 +1,146 @@
+ main :-
+    retractall(asked(_,_)),
+    fault(Problem),
+    !,
+    nl,
+    write('The problem is '), write(Problem), write(.), nl.
+main :-
+    nl,
+    write('The problem cannot be recognized.'), nl.
+
+problem(disc_format) :-
+    query('Does the computer show error cannot format').
+
+problem(boot_failure) :-
+    query('Does the computer show boot failure').
+
+problem(bad_sector) :-
+    query('Does the computer show bad sector error').
+
+problem(cannot_read) :-
+    query('Does the computer show cannot read from specified device').
+
+problem(long_beep) :-
+    query('Is there a long beep during bootup').
+
+problem(short_beep) :-
+    query('Is there a short beep during bootup').
+
+problem(two_long_beeps) :-
+    query('Are there two long beeps during bootup').
+
+problem(two_short_beeps) :-
+    query('Are there two short beeps during bootup').
+
+problem(blank_display) :-
+    query('Is there a blank display during bootup').
+
+problem(repeating_short_beeps) :-
+    query('Are there repeating short beeps during bootup').
+
+problem(continuous_beeps) :-
+    query('Is there a continuous beep during bootup').
+
+problem(no_beep) :-
+    query('Is there a beep during bootup').
+
+problem(not_printing) :-
+    query('Is there a problem with printing').
+
+problem(missing_dots) :-
+    query('Is there a missing character during printing').
+
+problem(non_uniform_printing) :-
+    query('Is there uniform printing').
+
+problem(spread_ink) :-
+    query('Is there spreading of ink during printing').
+
+problem(paper_jam) :-
+    query('Is there a paper jam during printing').
+
+problem(out_of_paper) :-
+    query('Is there out-of- paper error during printing').
+
+problem(increasing_of_temperature):-
+    query('The temperature of the video card or processor is increasing').
+
+problem(no_audio):-
+    query('You have no audio').
+
+problem(program_closes):-
+    query('The program closes for no reason').
+
+problem(productivity_drops_sharply):-
+    query('The computer productivity drops sharply').
+
+fault(audio_card_broken):-
+    problem(program_closes),
+    problem(no_audio).
+
+fault(computer_mining):-
+    problem(increasing_of_temperature),
+    problem(program_closes),
+    problem(productivity_drops_sharply).
+
+fault(computer_wont_turn_on) :-
+    problem(no_beep),
+    problem(repeating_short_beeps),
+    problem(blank_display).
+
+fault(system_overheating) :-
+    problem(blank_display),
+    problem(repeating_short_beeps),
+    problem(continuous_beeps).
+
+fault(power_supply) :-
+    problem(repeating_short_beeps),
+    problem(continuous_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(display_adapter) :-
+    problem(long_beep),
+    problem(two_short_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(motherboard) :-
+    problem(long_beep),
+    problem(short_beep).
+
+fault(hard_disc) :-
+    problem(two_short_beeps),
+    problem(blank_display).
+
+fault(booting_problem) :-
+    problem(bad_sector),
+    problem(boot_failure).
+
+fault(floppy_disk_unusable) :-
+    problem(bad_sector),
+    problem(cannot_read),
+    problem(disc_format).
+
+fault(printer_head) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(nonuniform_printing).
+
+fault(ribbon) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(spread_ink).
+
+fault(paper) :-
+    problem(not_printing),
+    problem(paper_jam),
+    problem(out_of_paper).
+
+query(Prompt) :-
+    (   asked(Prompt, Reply) -> true
+    ;   nl, write(Prompt), write(' (y/n)? '),
+        read(X),(X = y -> Reply = y ; Reply = n),
+	assert(asked(Prompt, Reply))
+    ),
+    Reply = y.
